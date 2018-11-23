3e4081d48029c7eec33300e04354dfe795c4c5d4
notmyfault02 <rayray.yoyo@gmail.com>
2018-11-23 17:08:47 +0900

    제네릭 메소드

---
+++ 제네릭/BoxingMethodExample.java
@@ -0,0 +1,11 @@
+package Generic;
+
+public class BoxingMethodExample {
+    public static void main(String[] args) {
+        //Box<Integer> box1 = Util.<Integer>boxing(100);  //타입 파라미터를 명시적으로 Integer로 지정
+        //int intValue = box1.get();
+
+        //Box<String> box2 = Util.boxing("홍길동");
+        //String strValue = box2.get();
+    }
+}
